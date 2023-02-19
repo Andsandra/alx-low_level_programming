bdefb9e48d715806e7f7469b0831a264f90b432d
Andsandra <muchangisandra8@gail.com>
2023-02-19 03:08:40 -0800

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game mandatory

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of three single-digit numbers
+ * separated by ", " and printed in ascending order
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
     char c;
@@ -10,7 +19,3 @@ int main(void)
 
     return 0;
 }
-
-
-
-
