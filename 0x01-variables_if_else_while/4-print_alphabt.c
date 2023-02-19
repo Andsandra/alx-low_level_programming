57b9cd4e0c4f0849a80801e3948171c6b5580bda
Andsandra <muchangisandra8@gail.com>
2023-02-19 04:15:20 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of three single-digit numbers
+ * separated by ", " and printed in ascending order
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    char c;
+	char c;
 
-    for (c = 'a'; c <= 'z'; ++c)
-    {
-        if (c != 'e' && c != 'q')
-            putchar(c);
-    }
-    putchar('\n');
-
-    return 0;
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
-
