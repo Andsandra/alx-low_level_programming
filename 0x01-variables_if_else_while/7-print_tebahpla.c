8e32312839f1371ffd9fcd31fa19b54d0d6b5f1f
Andsandra <muchangisandra8@gail.com>
2023-02-20 02:21:09 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: Prints all possible combinations of three single-digit numbers
- * separated by ", " and printed in ascending order
+ * main - prints the lowercase alphabet in reverse, followed by a new line
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 
-
 {
-	int i;
-	for (i = 122; i >= 97; i--)
+	char c = 'z';
+	
+	while (c >= 'a')
 	{
-		putchar(i);
+		
+		putchar(c)
+			c--;
 	}
 	putchar('\n');
-	return (0);
+	return 0;
 }
