049b502fc48c7d582c75854a8e404c71fcef5db7
Andsandra <muchangisandra8@gail.com>
2023-02-20 08:37:49 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,17 +5,17 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    int i;
-
-    for (i = 0; i < 10; i++)
-        putchar(i + '0');
-    for (i = 'a'; i <= 'f'; i++)
-        putchar(i);
-
-    putchar('\n');
-
-    return (0);
+	int i;
+	
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+	
+	putchar('\n');
+	
+	return (0);
 }
-
