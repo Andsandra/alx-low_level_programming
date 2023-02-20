cd7230110de6dec7509e5f95d44ebbfcc502e1b3
Andsandra <muchangisandra8@gail.com>
2023-02-20 01:00:26 -0800

    7. Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
-int main(void)
-{
-  int i;
-  for (i = 122; i >= 97; i--)
-  {
-    putchar(i);
-  }
-  putchar('\n');
-  return (0);
-}
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of three single-digit numbers
+ * separated by ", " and printed in ascending order
+ *
+ * Return: Always 0 (Success)
+ */
 
+int main(void)
+
+{
+	int i;
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
