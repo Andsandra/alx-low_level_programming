b0651a22202beda9a745f263997c8144c6a47f54
Andsandra <muchangisandra8@gail.com>
2023-02-27 16:14:35 -0800

    10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,41 +1,41 @@
 #include "main.h"
 
 /**
- *_atoi - Convert a string to an integer.
- *@s: The pointer to convert
- *Return: A integer
+ *_atoi - converts a string to an integer
+ *@s: pointer to string to be converted
+ *
+ *Return: the converted integer
  */
-
 int _atoi(char *s)
-
 {
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
+	int c = 0; /*index for iterating over the string */
+	unsigned int ni = 0; /*holds the converted integer */
+	int min = 1; /*used to determine if number is negative */
+	int isi = 0; /*used to determine if there are any integers in the string */
 
-	while (s[c])
+	while (s[c]) /*loop until end of string is reached */
 	{
-		if (s[c] == 45)
+		if (s[c] == 45) /*check if '-' character is present */
 		{
-			min *= -1;
+			min *= -1; /*set the value of min to -1 to indicate negative number */
 		}
 
-		while (s[c] >= 48 && s[c] <= 57)
+		while (s[c] >= 48 && s[c] <= 57) /*loop through digits */
 		{
-			isi = 1;
-			ni = (ni *10) + (s[c] - '0');
+			isi = 1; /*set isi to 1 to indicate integer is present */
+			ni = (ni *10) + (s[c] - '0'); /*convert char to integer and add to ni */
 			c++;
 		}
 
-		if (isi == 1)
+		if (isi == 1) /*check if an integer has been found */
 		{
-			break;
+			break; /*exit loop */
 		}
 
-		c++;
+		c++; /*increment index */
 	}
 
-	ni *= min;
-	return (ni);
+	ni *= min; /*multiply ni by min to get the correct sign */
+
+	return (ni); /*return the converted integer */
 }
